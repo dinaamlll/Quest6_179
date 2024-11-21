97bc5cf2ed96af76ed3f5dcb3715776fdf733e57
dinaamlll <dinamaliaa17@gmail.com>
2024-11-21 10:17:18 +0700

    membuat Column dengan modifier fillMaxSize() dan background yang diambil dari sumber daya warna dengan colorResource(id = R.color.primary)

--- app/src/main/java/com/example/navigationmultipledata_179/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationmultipledata_179/ui/view/screen/RencanaStudyView.kt
@@ -1,10 +1,31 @@
 package com.example.navigationmultipledata_179.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigationmultipledata_179.R
 import com.example.navigationmultipledata_179.model.Mahasiswa
 
 @Composable
@@ -12,15 +33,21 @@ fun RencanaStudyView (
     mahasiswa: Mahasiswa,
     onSubmitButtonClicked: (MutableList<String>) ->Unit,
     onBackButtonClicked: () -> Unit
-){
+) {
     var chosenDropdown by remember {
         mutableStateOf("")
     }
     var checked by remember {
-        mutableStateOf(false)}
+        mutableStateOf(false)
+    }
     var pilihanKelas by remember {
         mutableStateOf("")
     }
-    var listData: MutableList<String> = mutableListOf(chosenDropdown,pilihanKelas)
-
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ) {
     }
+}
