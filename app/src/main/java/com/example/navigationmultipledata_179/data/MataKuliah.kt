8fec8317c420b3157d96a9f2bba8fd32ec3dbca4
dinaamlll <dinamaliaa17@gmail.com>
2024-11-21 08:43:20 +0700

    membuat objek MataKuliah dengan daftar tetap (options) berisi nama-nama mata kuliah.

--- app/src/main/java/com/example/navigationmultipledata_179/data/MataKuliah.kt
+++ app/src/main/java/com/example/navigationmultipledata_179/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.navigationmultipledata_179.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pemrograman Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
 }
