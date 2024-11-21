e31b5851a8cef6d205e42f901ed3e345367f718f
dinaamlll <dinamaliaa17@gmail.com>
2024-11-21 09:26:36 +0700

    membuat variabel chosenDropdwon, checked, pilihanKelas, dan listData (berisi chosenDropdown,pilihanKelas)

--- app/src/main/java/com/example/navigationmultipledata_179/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationmultipledata_179/ui/view/screen/RencanaStudyView.kt
@@ -1,6 +1,10 @@
 package com.example.navigationmultipledata_179.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.navigationmultipledata_179.model.Mahasiswa
 
 @Composable
@@ -9,5 +13,14 @@ fun RencanaStudyView (
     onSubmitButtonClicked: (MutableList<String>) ->Unit,
     onBackButtonClicked: () -> Unit
 ){
+    var chosenDropdown by remember {
+        mutableStateOf("")
+    }
+    var checked by remember {
+        mutableStateOf(false)}
+    var pilihanKelas by remember {
+        mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown,pilihanKelas)
 
-}
+    }
