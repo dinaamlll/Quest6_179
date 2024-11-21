47327b70a85f675bf90c9467b8a04e110caf384e
dinaamlll <dinamaliaa17@gmail.com>
2024-11-21 23:51:56 +0700

    Menambahkan fitur dropdown menu menggunakan OutlinedTextField, memungkinkan pemilihan nilai dari daftar pilihan yang ditampilkan di bawah kotak teks.

--- app/src/main/java/com/example/navigationmultipledata_179/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/navigationmultipledata_179/ui/widget/DynamicSelectedField.kt
@@ -1,11 +1,18 @@
 package com.example.navigationmultipledata_179.ui.widget
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 
 @Composable
 fun DynamicSelectTextField(
@@ -16,4 +23,26 @@ fun DynamicSelectTextField(
     modifier: Modifier = Modifier){
 var expanded by remember {
     mutableStateOf(false)}
-}
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded},
+        modifier = modifier
+    ){
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = label) },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
+                }
+            }
+
+
