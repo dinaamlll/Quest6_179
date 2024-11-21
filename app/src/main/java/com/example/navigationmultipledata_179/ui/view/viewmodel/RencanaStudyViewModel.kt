7b55c3d4c88c2deb33e35e64b0a863a50e832d1c
dinaamlll <dinamaliaa17@gmail.com>
2024-11-22 00:07:17 +0700

    Menambahkan ViewModel RencanaStudyViewModel untuk mengelola data KRS (Mata Kuliah dan Kelas) menggunakan StateFlow.

---
+++ app/src/main/java/com/example/navigationmultipledata_179/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.navigationmultipledata_179.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.navigationmultipledata_179.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String) {
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>) {
+        _krsState.update { status ->
+            status.copy(
+                namaMK = ls[0],
+                kelas = ls[1]
+            )
+        }
+    }
+}
