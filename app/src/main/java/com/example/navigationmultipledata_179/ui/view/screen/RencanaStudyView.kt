91799f53b5be6e77f1cc04feb9816e1cad77d02e
dinaamlll <dinamaliaa17@gmail.com>
2024-11-21 13:04:42 +0700

    DynamicSelectTextField untuk memilih mata kuliah dari daftar yang telah ditentukan, dan nilai yang dipilih akan disimpan di chosenDropdown.

--- app/src/main/java/com/example/navigationmultipledata_179/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationmultipledata_179/ui/view/screen/RencanaStudyView.kt
@@ -117,6 +117,16 @@ fun RencanaStudyView (
                     fontWeight = FontWeight.Light
                 )
                 Spacer(modifier = Modifier.padding(8.dp))
+                DynamicSelectTextField(
+                    selectedValue = chosenDropdown,
+                    options = MataKuliah.options,
+                    label = "Mata Kuliah",
+                    onValueChangedEvent = {
+                        chosenDropdown = it
+                    }
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+
             }
         }
     }
