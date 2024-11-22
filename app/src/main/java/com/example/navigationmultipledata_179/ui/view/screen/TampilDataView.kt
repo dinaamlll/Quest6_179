be918da1476997cb0b11e75b043511a0f1e4573e
dinaamlll <dinamaliaa17@gmail.com>
2024-11-22 13:08:18 +0700

    finish

--- app/src/main/java/com/example/navigationmultipledata_179/ui/view/screen/TampilDataView.kt
+++ app/src/main/java/com/example/navigationmultipledata_179/ui/view/screen/TampilDataView.kt
@@ -1,4 +1,4 @@
-package com.example.multipledata.ui.view.screen
+package com.example.navigationmultipledata_179.ui.view.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
