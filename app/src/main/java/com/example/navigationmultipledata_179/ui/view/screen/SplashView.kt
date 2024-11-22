afebfab66bbcbdff79bad1865488e0ac2eb44403
dinaamlll <dinamaliaa17@gmail.com>
2024-11-22 13:06:47 +0700

    menambahkan teks "Mulai" dan memanggil fungsi onMulaiButton() saat ditekan

--- app/src/main/java/com/example/navigationmultipledata_179/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/navigationmultipledata_179/ui/view/screen/SplashView.kt
@@ -1,9 +1,12 @@
 package com.example.navigationmultipledata_179.ui.view.screen
 
+
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -21,19 +24,32 @@ import com.example.navigationmultipledata_179.R
 
 
 @Composable
-fun SplashView (
-    onMulaiButton: ()->Unit,
-){
-    Column (modifier = Modifier
-        .fillMaxSize()
-        .background(color = colorResource(id = R.color.primary)),
+fun SplashView(
+    onMulaiButton: () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    ) {
         Image(
             painter = painterResource(R.drawable.logoumy),
             contentDescription = "",
             modifier = Modifier.size(150.dp)
         )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Button(
+            onClick = {
+                onMulaiButton() // Pastikan fungsi dipanggil
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            Text(text = "Mulai")
+        }
     }
 }
+
