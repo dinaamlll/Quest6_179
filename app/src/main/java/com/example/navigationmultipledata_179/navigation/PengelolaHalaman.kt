616145d147e7f90b391294f80180f72ccecc92f5
dinaamlll <dinamaliaa17@gmail.com>
2024-11-22 12:15:02 +0700

    menambahkan enum class Halaman yang isinya Peminatan dan TampilKrs

--- app/src/main/java/com/example/navigationmultipledata_179/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/navigationmultipledata_179/navigation/PengelolaHalaman.kt
@@ -4,20 +4,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.navigationmultipledata_179.model.Mahasiswa
+import com.example.multipledata.ui.view.screen.TampilView
 import com.example.navigationmultipledata_179.ui.view.screen.MahasiswaFormView
+import com.example.navigationmultipledata_179.ui.view.screen.RencanaStudyView
 import com.example.navigationmultipledata_179.ui.view.screen.SplashView
 import com.example.navigationmultipledata_179.ui.view.viewmodel.MahasiswaViewModel
-import java.security.AccessController
-import androidx.lifecycle.viewmodel.compose.viewModel as viewModel
+import com.example.navigationmultipledata_179.ui.view.viewmodel.RencanaStudyViewModel
 
-enum class Halaman{
+
+enum class Halaman {
     Splash,
-    Mahasiswa
+    Mahasiswa,
+    Peminatan,
+    TampilKrs
 }
 
 
@@ -25,23 +30,49 @@ enum class Halaman{
 fun MahasiswaApp(
     modifier: Modifier = Modifier,
     mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    RencanaStudyViewModel: RencanaStudyViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ) {
     val mahasiswaUiState = mahasiswaViewModel.mahasiswaUiState.collectAsState().value
+    val rencanaStudiUiState = RencanaStudyViewModel.krsStateUi.collectAsState().value
+
 
     NavHost(
         navController = navController,
         startDestination = Halaman.Splash.name,
-        modifier = Modifier.padding()
+        modifier = modifier.padding()
     ) {
-        composable(route = Halaman.Splash.name) {
-            SplashView(onMulaiButton = {
-                navController.navigate(
-                    Halaman.Mahasiswa.name
-                )
-            })
+        composable(
+            route = Halaman.Splash.name
+        ){
+            SplashView  (
+                onMulaiButton = {
+                    navController.navigate(Halaman.Mahasiswa.name)
+                })
         }
+
+
         composable(route = Halaman.Mahasiswa.name) {
+            MahasiswaFormView (
+                onSubmitButtonClicked = {
+                    mahasiswaViewModel.saveDataMahasiswa(it)
+                    navController.navigate(Halaman.Peminatan.name)},
+                onBackButtonClicked = {navController.popBackStack()}
+            )
+        }
+
+
+        composable(route = Halaman.Peminatan.name) {
+            RencanaStudyView (
+                mahasiswa = mahasiswaUiState,
+                onSubmitButton = {
+                    RencanaStudyViewModel.saveDataKRS(it)
+                    navController.navigate(Halaman.TampilKrs.name)
+                },
+                onbackbuttonClicked = {
+                    navController.popBackStack()
+                }
+            )
+        }
         }
     }
-}
